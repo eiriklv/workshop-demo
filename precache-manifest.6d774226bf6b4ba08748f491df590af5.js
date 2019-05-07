26ad58fba87f2886e420ca13f02fefa294fb272f
Eirik Vullum <evullum@gmail.com>
2019-05-07 12:23:43 -0500

    Updates

---
+++ precache-manifest.6d774226bf6b4ba08748f491df590af5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f0ec4dd765008b0adfc4e78837e806",
+    "url": "/workshop-demo/index.html"
+  },
+  {
+    "revision": "f09873708f33876b4a41",
+    "url": "/workshop-demo/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/workshop-demo/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f09873708f33876b4a41",
+    "url": "/workshop-demo/static/js/main.102125dc.chunk.js"
+  },
+  {
+    "revision": "822beb640cf238fde6a8",
+    "url": "/workshop-demo/static/js/runtime~main.e11297a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/workshop-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
